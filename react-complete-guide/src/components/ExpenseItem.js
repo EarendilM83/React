4f07886c4f8091914787f0600de94ff3d38abbf8
EarendilM83 <nukri.tusishvili@gmail.com>
2021-10-14 02:50:56 +0400

    make data dynamic

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,23 @@
-function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+import ExpenseDate from "./ExpenseDate";
+import "./styles/ExpensesItem.css";
+
+function ExpenseItem(props) {
+  //   const expenseDate = new Date(2021, 2, 28);
+  //   const expenseTitle = "Healthcare";
+  //   const expenseAmount = 294.67;
+
+  return (
+    <div className="expense-item">
+      {/* start element */}
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
+      </div>
+
+      {/* end element */}
+    </div>
+  );
 }
 
 export default ExpenseItem;
