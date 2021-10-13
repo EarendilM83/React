a9217f16f64b871a2e976660186c941d87596e55
EarendilM83 <nukri.tusishvili@gmail.com>
2021-10-13 23:59:48 +0400

    react project added

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Hello Fucking world, I'm barny პატარების მეგობარი!</h2>
+      <p>მარცვლეულის ქვეყნიდან მოსული</p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
