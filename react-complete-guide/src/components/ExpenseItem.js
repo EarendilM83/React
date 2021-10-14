e2de8ae9ec8a533e1d30a2396df8384533cb9915
EarendilM83 <nukri.tusishvili@gmail.com>
2021-10-15 02:13:27 +0400

    rough click reference added

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -6,6 +6,11 @@ function ExpenseItem(props) {
   //   const expenseDate = new Date(2021, 2, 28);
   //   const expenseTitle = "Healthcare";
   //   const expenseAmount = 294.67;
+  let title = props.title;
+  const clickHandler = () => {
+    title = "Updated";
+    console.log("updated");
+  };
 
   return (
     <Card className="expense-item">
@@ -15,7 +20,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
-
+      <button onClick={clickHandler}>Click fkng me</button>
       {/* end element */}
     </Card>
   );
