2975b7cfb5f1d3933b284eab91be442973ae6142
EarendilM83 <nukri.tusishvili@gmail.com>
2021-10-15 22:04:25 +0400

    use state added

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 import "./styles/ExpensesItem.css";
 
 function ExpenseItem(props) {
-  //   const expenseDate = new Date(2021, 2, 28);
-  //   const expenseTitle = "Healthcare";
-  //   const expenseAmount = 294.67;
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    title = "Updated";
+    setTitle("updated!");
     console.log("updated");
   };
 
@@ -17,7 +17,7 @@ function ExpenseItem(props) {
       {/* start element */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
       <button onClick={clickHandler}>Click fkng me</button>
